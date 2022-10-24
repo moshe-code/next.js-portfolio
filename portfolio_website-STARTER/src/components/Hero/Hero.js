a529429b42ca78de2e990bcf90fd877e7d19d7ac
Moshe <moshesweiner@.com>
2022-10-24 17:07:13 +0300

    init

---
+++ portfolio_website-STARTER/src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br/>
+        Mypersonal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Hey I am a full-stack developer reday to take on your next project! Lets get to work.
+      </SectionText>
+      <Button onClick={()=> window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
